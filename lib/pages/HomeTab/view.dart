81c30320e129385e8292a85a969be86e92a80507
fong <dart@qq.com>
2024-04-09 16:02:41 +0800

    "aaa"

---
+++ lib/pages/HomeTab/view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:extended_tabs/extended_tabs.dart';
+
+final lengthProvider = Provider<int>((ref) {
+  return 3;
+});
+
+class HomeTabPage extends ConsumerStatefulWidget {
+  const HomeTabPage({super.key});
+
+  @override
+  ConsumerState<HomeTabPage> createState() => _HomeTabPageState();
+}
+
+class _HomeTabPageState extends ConsumerState<HomeTabPage>
+    with SingleTickerProviderStateMixin {
+  late final AutoDisposeProvider<TabController>
+      recentWatchScreenTabStateTestProvider;
+
+  @override
+  void initState() {
+    recentWatchScreenTabStateTestProvider = Provider.autoDispose(
+      (ref) => TabController(
+        length: ref.watch(lengthProvider),
+        vsync: this,
+      ),
+    );
+    super.initState();
+  }
+
+  List tabs = ["新闻", "历史", "图片"];
+
+  @override
+  Widget build(BuildContext context) {
+    final tabController = ref.watch(recentWatchScreenTabStateTestProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "爱动漫",
+          style: TextStyle(fontSize: 16),
+        ),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: tabs.map((e) => Tab(text: e)).toList(),
+        ),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    final tabController = ref.watch(recentWatchScreenTabStateTestProvider);
+    return TabBarView(controller: tabController, children: [
+      Container(
+        child: Text("sdsdsd1"),
+      ),
+      Container(
+        child: Text("sdsdsd2"),
+      ),
+      Container(
+        child: Text("sdsdsd3"),
+      ),
+    ]);
+  }
+}
