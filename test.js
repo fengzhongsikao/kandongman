fdb62e08b0a8bb8606f5bdd2c8aea20fe0215da9
fong <dart@qq.com>
2024-04-09 17:20:24 +0800

    "结构"

---
+++ test.js
@@ -0,0 +1,29 @@
+// 轮胎 l   火花 h   机油 j
+
+let l1 = {
+    kind:41,
+    serviceType:1
+}
+let l2 = {
+    kind:41,
+    serviceType:1
+}
+
+let l3 = {
+     kind:41,
+    serviceType:2
+}
+
+let h1 = {
+    kind:1
+}
+let h2 = {
+    kind:1
+}
+
+let j = {
+    kind:2
+}
+
+
+let result = [{kind:41,collection:true},{kind:1,collection:false},{kind:2,collection:false}]
