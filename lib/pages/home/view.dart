9dbb942a5a8a031fc6929b7a2312099cd56d6f76
fong <dart@qq.com>
2024-04-10 16:39:46 +0800

    添加button

--- lib/pages/home/view.dart
+++ lib/pages/home/view.dart
@@ -3,6 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:extended_tabs/extended_tabs.dart';
 
+import 'package:kandongman/widgets/button.dart';
+import 'package:http/http.dart' as http;
+// import 'dart:html';
+
 final lengthProvider = Provider<int>((ref) {
   return 3;
 });
@@ -30,7 +34,7 @@ class _HomeTabPageState extends ConsumerState<Home>
     super.initState();
   }
 
-  List tabs = ["新闻", "历史", "图片"];
+  List tabs = ["推荐", "日漫", "国漫"];
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +60,26 @@ class _HomeTabPageState extends ConsumerState<Home>
     final tabController = ref.watch(recentWatchScreenTabStateTestProvider);
     return TabBarView(controller: tabController, children: [
       Container(
-        child: Text("sdsdsd1"),
+        child: Column(
+          children: [
+            myCustomButton(
+              onTap: () async {
+                var url = Uri.https('https://9ciyuan.com');
+                var response = await http.get(url);
+                print('Response status: ${response.statusCode}');
+                print('Response body: ${response.body}');
+
+                // print(await http.read(Uri.https('example.com', 'foobar.txt')));
+                // var response = await http.get('xxx.html');
+                // Document document = parse(response.body);
+                // print("aaaa----${document}");
+              },
+              title: "点击",
+              width: 100,
+              height: 50,
+            ),
+          ],
+        ),
       ),
       Container(
         child: Text("sdsdsd2"),
